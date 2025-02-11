b9f6aba4d585362455647167969193ccd474c0bb
Alex <alexrivada@gmail.com>
2025-02-11 12:32:53 +0100

    SDI-2425-613-sesion4-ejercicio1y2

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -54,7 +54,14 @@ public class UsersController {
     }
     @RequestMapping(value = "/user/edit/{id}", method = RequestMethod.POST)
     public String setEdit(@PathVariable Long id, @ModelAttribute User user) {
-        usersService.addUser(user);
+        User originalUser = usersService.getUser(id);
+
+        // Modificar solo los campos permitidos
+        originalUser.setDni(user.getDni());
+        originalUser.setName(user.getName());
+        originalUser.setLastName(user.getLastName());
+
+        usersService.addUser(originalUser);
         return "redirect:/user/details/" + id;
     }
 
@@ -84,5 +91,11 @@ public class UsersController {
     @RequestMapping(value = { "/home" }, method = RequestMethod.GET)
     public String home() {
         return "home";
+    }
+
+    @RequestMapping("/user/list/update")
+    public String updateList(Model model) {
+        model.addAttribute("usersList", usersService.getUsers());
+        return "user/list :: usersTable";
     }
 }
---
+++ src/main/java/com/uniovi/notaneitor/validators/AddMarkValidator.java
@@ -0,0 +1,32 @@
+package com.uniovi.notaneitor.validators;
+
+import com.uniovi.notaneitor.entities.Mark;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class AddMarkValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Mark.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Mark mark = (Mark) target;
+
+        // Validación de la descripción
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "Error.empty");
+        if (mark.getDescription().length() < 20) {
+            errors.rejectValue("description", "Error.mark.description.length");
+        }
+
+        // Validación de la puntuación
+        if (mark.getScore() < 0 || mark.getScore() > 10) {
+            errors.rejectValue("score", "Error.mark.score.range");
+        }
+    }
+}
